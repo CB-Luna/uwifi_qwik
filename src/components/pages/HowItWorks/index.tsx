0ece100d6319da91bb942d2170c1ff39a02e09cf
Fernando Cano <fernado.cano@cbluna.com>
2024-02-01 09:44:04 -0800

    Se termino la pagina how it works

--- src/components/pages/HowItWorks/index.tsx
+++ src/components/pages/HowItWorks/index.tsx
@@ -30,7 +30,7 @@ export const HowItWorks = component$(() => {
     description: "A few simple taps and you can control which devices connect to Wi-Fi and when. You have the power."
   }]
 
-  return <div class="flex flex-col p-10 bg-black" id="header">
+  return <div class="flex flex-col  bg-black justify-center items-center" id="header">
     <div class="flex flex-col text-[42px] md:items-start items-center font-bold justify-center">
       <h1><span class="text-primary-green">How</span> it works?</h1>
       <p>Simple set up in less than <span class="text-primary-green">15 minutes!</span></p>
@@ -38,13 +38,46 @@ export const HowItWorks = component$(() => {
 
     <SectionSteps />
 
-    <div class="flex flex-col text-[42px] md:items-start my-5 items-center font-bold justify-center">
+    <div class="flex flex-col p-10 md:text-[42px] text-3xl md:items-start my-5 items-center font-bold justify-center">
       <h2><span class="text-primary-green">U-Wifi</span> Mobile App</h2>
       <p>Take control of your home internet with one <span class="text-secondary-purple">simple app</span> </p>
     </div>
 
     {dataParagraph.map((paragraph: any, key: any) => (
-      <Paragraph image={paragraph['image']} description={paragraph['description']} title={paragraph['title']} direction={key % 2 == 0 ? "left" : "rigth"} />
+      <Paragraph key={key} image={paragraph['image']} description={paragraph['description']} title={paragraph['title']} direction={key % 2 == 0 ? "left" : "rigth"} />
     ))}
+
+    <div id="banner" class="w-full h-[200px] gap-4 flex flex-col justify-center items-center" style={`background-image:url("https://uwif-astro-web.vercel.app/image/baner.png");
+      background-size:cover;
+      background-repeat: no-repeat;`}>
+      <h1 class="text-white font-extrabold text-center md:text-[40px] text-3xl">A WINDOW INTO 5G WIRELESS INTERNET</h1>
+      <p class="text-center md:text-2xl text-xl">How 5G wireless technology connects you to the world from the comfort of your home.</p>
+    </div>
+
+    <div id="image" class="w-3/4 h-[150px] my-10 flex justify-center items-center">
+      <img class="h-full w-full object-contain" src="https://uwif-astro-web.vercel.app/image/conection.svg" alt=""
+        width="934" height="155" />
+    </div>
+
+    <div class="p-10 flex md:flex-row flex-col items-center justify-center gap-16">
+      <div class="flex flex-col items-center justify-center gap-6">
+        <h2 class="md:text-3xl text-xl font-bold text-left">How our 5G internet is different</h2>
+        <p class="md:text-2xl text-xl text-justify">While cable internet relies on wired connections, UWIFI Internet uses the power of over-the-air 5G to provide reliable connectivity where wires can’t reach. Plus, we won’t tangle you up in long-term contracts.</p>
+      </div>
+      <div class="flex flex-col items-center justify-center gap-6">
+        <h2 class="md:text-3xl text-xl font-bold text-left">How our 5G home internet works</h2>
+        <p class="md:text-2xl text-xl text-justify">UWIFI 5G network is built on a series of cell sites that use radio waves to send and receive data. Your gateway converts the nearest 5G signal into Wi-Fi, allowing you to access the internet wirelessly.</p>
+      </div>
+    </div>
+
+    <h2 class="md:text-[40px] text-3xl text-center font-bold">Does the placement of your 5G gateway matter?</h2>
+    <div class="flex flex-wrap gap-5 items-center justify-center p-10 my-5">
+      <div class="flex flex-col md:w-2/3 w-full justify-center items-start gap-4">
+        <h2 class="md:text-3xl text-xl font-bold">How our 5G internet is different</h2>
+        <p class="md:text-2xl text-lg text-justify">window facilitates a strong connection to the closest 5G cell tower or data hub, resulting in faster download and upload speeds, lower latency, and increased capacity compared to locations with weaker connections.</p>
+        <MainButton isHollow>Device Details</MainButton>
+      </div>
+      <div class="md:w-[20%] w-2/3 flex flex-row bg-slate-400 h-[300px] rounded-lg"></div>
+    </div>
   </div>;
 });
--- src/components/pages/HowItWorks/sectionSteps.tsx
+++ src/components/pages/HowItWorks/sectionSteps.tsx
@@ -2,27 +2,27 @@ import { component$ } from "@builder.io/qwik";
 import { MainButton } from "~/components/MainButton";
 
 export const SectionSteps = component$(() => {
-  return <div class="flex flex-wrap justify-center items-center my-5 gap-4">
-  <div class="w-3/4 flex flex-col gap-5 justify-start items-start">
+  return <div class="flex flex-wrap p-8 justify-center items-center my-5 gap-4">
+  <div class="md:w-3/4 w-full flex flex-col gap-2 justify-start items-start">
     <div class="flex flex-row justify-start items-center gap-5">
         <svg class="object-cover w-[clamp(50px,5vw,150px)] h-[clamp(50px,10vw,180px)]" alt-arial="mark icon" xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 131" fill="none" data-astro-cid-cdiqt7qs=""> <path d="M49.68 12.1316C39.432 12.1316 29.6038 16.4613 22.3574 24.1681C15.111 31.8749 11.04 42.3276 11.04 53.2267C11.04 70.0288 20.8766 86.2379 31.6738 98.7954C37.1975 105.189 43.2212 111.073 49.68 116.384C56.1387 111.077 62.1624 105.197 67.6862 98.8071C78.4834 86.2379 88.32 70.0347 88.32 53.2267C88.32 42.3276 84.249 31.8749 77.0026 24.1681C69.7562 16.4613 59.928 12.1316 49.68 12.1316ZM49.68 130.803L46.5502 128.513L46.5336 128.501L46.5005 128.472L46.3901 128.39L45.9761 128.079C43.6904 126.312 41.4556 124.471 39.2748 122.56C33.6851 117.677 28.4228 112.384 23.5262 106.721C12.2434 93.5939 0 74.5786 0 53.2267C1.96337e-07 39.2136 5.23413 25.7744 14.5509 15.8656C23.8677 5.95683 36.504 0.390137 49.68 0.390137C62.856 0.390137 75.4923 5.95683 84.8091 15.8656C94.1259 25.7744 99.36 39.2136 99.36 53.2267C99.36 74.5786 87.1166 93.5998 75.8338 106.721C69.4409 114.116 62.4272 120.876 54.8798 126.916C54.2509 127.416 53.6161 127.907 52.9754 128.39L52.8595 128.472L52.8264 128.501L52.8154 128.507L49.68 130.797V130.803ZM49.68 41.4853C46.752 41.4853 43.9439 42.7223 41.8735 44.9243C39.8031 47.1262 38.64 50.1127 38.64 53.2267C38.64 56.3408 39.8031 59.3273 41.8735 61.5292C43.9439 63.7312 46.752 64.9682 49.68 64.9682C52.608 64.9682 55.4161 63.7312 57.4865 61.5292C59.5569 59.3273 60.72 56.3408 60.72 53.2267C60.72 50.1127 59.5569 47.1262 57.4865 44.9243C55.4161 42.7223 52.608 41.4853 49.68 41.4853ZM27.6 53.2267C27.6 46.9987 29.9263 41.0257 34.0671 36.6218C38.2079 32.2179 43.824 29.7438 49.68 29.7438C55.536 29.7438 61.1521 32.2179 65.2929 36.6218C69.4337 41.0257 71.76 46.9987 71.76 53.2267C71.76 59.4548 69.4337 65.4278 65.2929 69.8317C61.1521 74.2356 55.536 76.7097 49.68 76.7097C43.824 76.7097 38.2079 74.2356 34.0671 69.8317C29.9263 65.4278 27.6 59.4548 27.6 53.2267Z" fill="white" data-astro-cid-cdiqt7qs=""></path> </svg>
       
       <p class="font-extrabold text-[40px]">1</p>
-      <p class="text-3xl">Confirm internet coverage at your address</p>
+      <p class="md:text-3xl text-xl">Confirm internet coverage at your address</p>
     </div>
-    <div class="flex flex-row gap-5">
+    <div class="flex flex-row gap-5 justify-start items-center">
       <svg class="w-[clamp(50px,5vw,150px)] h-[clamp(50px,10vw,180px)]" alt-arial="Receive icon" xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 129 121" fill="none" data-astro-cid-cdiqt7qs=""> <path d="M52.2222 81.6459L69.5556 87.7659C69.5556 87.7659 112.889 78.5859 118.667 78.5859C124.444 78.5859 124.444 84.7059 118.667 90.8259C112.889 96.9459 92.6667 115.306 75.3333 115.306C58 115.306 46.4444 106.126 34.8889 106.126C23.3333 106.126 6 106.126 6 106.126" stroke="white" stroke-width="10.189" stroke-linecap="round" stroke-linejoin="round" data-astro-cid-cdiqt7qs=""></path> <path d="M6 73.9014C11.7778 67.3532 23.3333 57.531 34.8889 57.531C46.4444 57.531 73.8889 70.6273 78.2222 77.1755C82.5556 83.7236 69.5556 93.5458 69.5556 93.5458" stroke="white" stroke-width="10.189" stroke-linecap="round" stroke-linejoin="round" data-astro-cid-cdiqt7qs=""></path> <path d="M40.6665 35.7455V11.2655C40.6665 7.88552 43.2532 5.14551 46.4443 5.14551H115.778C118.969 5.14551 121.555 7.88552 121.555 11.2655V60.2255" stroke="white" stroke-width="10.189" stroke-linecap="round" stroke-linejoin="round" data-astro-cid-cdiqt7qs=""></path> <path d="M95.5554 5.14551H66.6665V32.6855H95.5554V5.14551Z" fill="bg-primaryColor" stroke="white" stroke-width="10.189" stroke-linecap="round" stroke-linejoin="round" data-astro-cid-cdiqt7qs=""></path> </svg>
       <p class="font-extrabold text-[40px]">2</p>
-      <p class="text-3xl">Receive your 5G Wi-Fi gateway device.</p>
+      <p class="md:text-3xl text-xl">Receive your 5G Wi-Fi gateway device.</p>
     </div>
-    <div class="flex flex-row gap-5">
+    <div class="flex flex-row gap-5 justify-start items-center">
       <img class="object-contain w-[clamp(50px,5vw,150px)] h-[clamp(50px,10vw,180px)]" src="https://uwif-astro-web.vercel.app/image/movilconlogo.png" alt="" width="284" height="522" />
 
       <p class="font-extrabold text-[40px]">3</p>
-      <p class="text-3xl">Download our app and get set up in minutes.</p>
+      <p class="md:text-3xl text-xl">Download our app and get set up in minutes.</p>
     </div>
   </div>
-  <div class="flex flex-col justify-center items-center gap-5">
+  <div class="flex flex-col md:w-1/5 w-1/3 justify-center items-center gap-5">
     <img class="w-3/4" src="https://uwif-astro-web.vercel.app/image/movilconlogo.png" alt="" 
     width="284" height="522"/>
     <MainButton isHollow link="https://uwif-astro-web.vercel.app/signup">Download App</MainButton>
